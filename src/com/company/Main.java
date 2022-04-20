5734ce3abf24ad6f10fcf99c4d675a36c77cd9c0
Danii <danimartinezortin@gmail.com>
2022-04-20 16:27:24 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.entities.Fitxa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("test");
+    }
+}
---
+++ src/com/company/entities/Fitxa.java
@@ -0,0 +1,26 @@
+package com.company.entities;
+
+public class Fitxa {
+    private String referencia, titol;
+
+    public Fitxa(String referencia, String titol) {
+        this.setReferencia(referencia);
+        this.setTitol(titol);
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+}
